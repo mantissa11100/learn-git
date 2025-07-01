430b2feb866b86d03f099b18d464c9907b5ca978
pongphet <pongphet7642@gmail.com>
2025-07-01 23:11:19 +0700

    save me

--- learngithub.c
+++ learngithub.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {   //data
     char name[3]="man";
+    bool status=true;
     //output
-    printf("Name = %s",name);
+    printf("Name = %s\n",name);
+    printf("Status = %d\n",status);
 
     return 0;
 }
